91872f8d5de891f9096d916afd715d551c58faeb
zeze1004 <thwjd2717@naver.com>
2021-02-13 03:20:26 +0900

    [auth] cookie-parser 없다는 오류 발생...

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,21 @@
 import { userModel } from "../user.model";
+import cookieParser from "cookie-parser";
 
-let 
+
+let auth = (req, res, next) => {
+	let token = req.cookies.x_auth;
+
+	userModel.findByToken(token)
+	.then((user) => {
+		if (!user)
+			return res.send('해당 유저가 존재하지 않습니다.');
+		req.token = token;
+		req.user = user;
+		next();
+	})
+	.catch((err) => {
+		throw err;
+	});
+};
+
+export { auth };
--- module/user/user.model.js
+++ module/user/user.model.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import cookieParser from "cookie-parser";
 
 const saltRounds = 10;
 
