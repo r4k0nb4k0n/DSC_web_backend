100139469a046f65813156480259d6b34abd2450
zeze1004 <thwjd2717@naver.com>
2021-02-12 19:02:57 +0900

    필요없는 주석 제거

--- module/user/user.controller.js
+++ module/user/user.controller.js
@@ -1,6 +1,5 @@
 import { userModel } from "./user.model";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
-// import { body, validationResult } from "validator";
 
 const userController = {};
 
@@ -16,7 +15,6 @@ userController.userCreate = async (req, res) => {
   } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      // .json({ error: error.toString() });
 			.send('중복된 이메일입니다.');
   }
 };
@@ -57,26 +55,6 @@ userController.userSession = async (req, user) => {
   }
 };
 
-// JWT
-/*
-userController.
-var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
-    });
-}));
-*/
 userController.update = async (req, res) => {
   try {
     let user = await userModel.findById(req.params.id);
